59373404148d4e621ecb76bad846e1ac10b46ac5
prakhar <prakharharshit.2003@gmail.com>
2024-03-01 17:23:42 +0530

    first commit on city repo

--- Flight_Search_Service/src/repository/city-repository.js
+++ Flight_Search_Service/src/repository/city-repository.js
@@ -1,27 +1,66 @@
 const {City}=require('../models/index');
 
 class CityRepository{
-    async createCity({name}){
+
+
+    async createCity({name}){  //{"name:"new delhi"}
+
         try{
-const city=await City.create({name});
-return city;
+            const city=await City.create({
+                name
+            });
+            return city;
         }
         catch(error){
+console.log("something went wrong in repositoryt");
 throw{error};
         }
     }
-    async deleteCity({cityid}){
-        try{
-await City.destroy({
+
+
+
+async deleteCity(cityId){
+    try{
+        await City.destroy({
+            where:{
+                id:cityId
+            }
+        });
+        return true;
+    }catch(error){
+        console.log("something went wrong in repositoryt");
+        throw{error};
+    }
+
+}
+async updateCity(cityId,data){
+try{
+const city=await City.update(data,{
     where:{
-        id:cityid
+        id:cityId
     }
 });
-        }
-        catch(error){
-            throw{error};
-        }
+return city;
+}
+catch(error){
+    console.log("something went wrong in repositoryt");
+    throw{error};
+}
+}
+
+async getCity(cityId){
+    try{
+const city=await City.findByPk(cityId);
+return City;
+    } catch(error){
+        console.log("something went wrong in repositoryt");
+throw{error};
     }
+
+}
+
+
+
 }
 module.exports=CityRepository;
 
