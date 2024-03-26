b7e5d1fae8049867c242780a91ff3b8bf08f83f6
prakhar <prakharharshit.2003@gmail.com>
2024-03-26 17:06:39 +0530

    added error codes enums

---
+++ Flight_Search_Service/src/utils/error-codes.js
@@ -0,0 +1,23 @@
+const ClientErrorCodes=Object.freeze({
+BAD_REQUEST:400,
+UNAUTHORISED:401,
+NOT_FOUND:404
+});
+
+const ServerErrorCodes=object.freeze({
+INTERNAL_SERVICE_ERROR:500,
+NOT_IMPLEMENTED:501
+
+});
+
+const SuccessCodes=Object .freeze({
+    OK:200,
+    CREATED:201
+});
+
+module.exports={
+    ClientErrorCodes,
+    ServerErrorCodes,
+    SuccessCodes
+
+}
